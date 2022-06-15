b119d5694e761f2e3f759b4d0f095ab5c711192e
wingcube <dykimoz@naver.com>
2022-06-16 06:29:03 +0900

    your grade and rank program example

---
+++ Hanseoprogram.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class HanseoProgram {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("진로과목 점수를 입력하세요 : ");
+		int grade = scan.nextInt();
+
+		if (grade >= 80) {
+			System.out.println("등급은 A 입니다.");
+		}
+		else if (grade >= 60) {
+			System.out.println("등급은 B 입니다.");
+		}
+		else if (grade >=40) {
+			System.out.println("등급은 C 입니다.");
+		}
+		else {
+			System.out.println("조금 더 노력하세요.");
+		}
+		
+			
+		scan.close();
+	}
+
+}
